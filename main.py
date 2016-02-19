838fb123e641734f8e9c5d49f973c18859b01706
jnwcki <jude.nowicki@gmail.com>
2016-02-19 14:00:40 -0500

    full name search with all stats

--- main.py
+++ main.py
@@ -7,7 +7,17 @@ while True:
     user_input = input("Search for player by name: ")
 
     cur.execute("SELECT * FROM player_stats WHERE LOWER(player_name) = LOWER(%s);", (user_input,))
-    print(cur.fetchall())
+
+    player_info = cur.fetchall()[0]
+    print("\nName: " + player_info[2])
+    print("Number: " + str(player_info[1]))
+    print("Position: " + player_info[3])
+    print("Age: " + str(player_info[4]))
+    print("Height: " + player_info[5])
+    print("Weight: " + str(player_info[6]))
+    print("Shoots/Catches: " + player_info[7])
+    print("Years Exp: " + player_info[8])
+    print("Birthdate: " + player_info[9] + "\n")
 
 
 
